a580e3d8305668905c40d19732f14d112b2499bc
kwintasha <80160659+kwintasha@users.noreply.github.com>
2021-03-06 14:56:00 +0100

    Add files via upload

---
+++ brouillon de la mort qui tue.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 14:44:05 2020
+
+@author: natas
+"""
+<a href="weeb_weeb.html">Acceuil</a>
+
+https://youtu.be/7A4IlwuuyyI aka Miss Kobayashi's Dragon Maid [OP_Opening]
+
+aside{ 
+position: absolute;
+top:0px;
+left:600px;
+width:200px;
+height:300px;
+background-color: #FFFBB3;
+}
+
+    footer{ 
+position: absolute;
+top:400px;
+left:0px;
+width:1024px;
+height:100px;
+background-color: pink;
+}
+
+aside{ 
+position: absolute;
+top:100px;
+left:1200px;
+width:300px;
+height:828px;
+background-color: #C9C9C9;
+}
+
+https://myanimelist.net/anime/33206/Kobayashi-san_Chi_no_Maid_Dragon?q=miss
+        <aside> <br> <div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/l0IxZkXQw9A7OqbbW" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/cute-moe-l0IxZkXQw9A7OqbbW"></a></p>
+
+        <div class="tenor-gif-embed" data-postid="16145976" data-share-method="host" data-width="100%" data-aspect-ratio="1.7978339350180503"><a href="https://tenor.com/view/kanna-kamui-kanna-kobayashi-miss-kobayashis-dragon-maid-kobayashi-san-chi-no-maid-dragon-anime-gif-16145976">Kanna Kamui Kanna Kobayashi GIF</a> from <a href="https://tenor.com/search/kannakamui-gifs">Kannakamui GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+
+<blockquote>
+    «&nbsp;Kobayashi lives alone in an apartment, until one day, Tooru appeared and they ended up living together. Tooru looks down on humans as inferior and foolish, but having been saved by Kobayashi-san, she does everything she can to repay the debt and help her with various things, although not everything goes according to plan.
+A mythical everyday life comedy about a hard working office lady living with a dragon girl.&nbsp;» </blockquote>
+
+<a class="kanna.jpg" data-lightbox="kanna.jpg" href="file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/kanna.jpg.png"><img class="example-image" alt="oi" src="file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/kanna.jpg.png"></a>
+
+
+<a class="kobayashi.png" data-lightbox="kobayashi.png" href="file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/kobayashi.png"><img class="example-image" alt="oi" src="file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/kobayashi.png"></a> </td></td>
+
+
+<a class="tohru.png" data-lightbox="tohru.png" href="file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/tohru.png"><img class="example-image" alt="oi" src="file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/tohru.png"></a> </td></td>
+
+
+
+
+<b>Fhána</b>
+(stylized as 
+ <b>fhána</b>
+ ) is a four-member 
+<a href="/wiki/Japanese_pop" class="mw-redirect" title="Japanese pop">Japanese pop</a>
+band that formed in 2012 as an 
+<a href="/wiki/Independent_music" title="Independent music">indie</a>
+ band. After releasing one album independently, they were signed in 2013 to 
+<a href="/wiki/Lantis_(company)" title="Lantis (company)">Lantis</a>
+. They released their debut single "Que Sera Sera" in August 2013.
+
+ <span style="font-weight: normal">  (<span lang="ja" title="Japanese language text">佐藤 純一</span>, <i lang="ja-Latn" title="Hepburn transliteration">Satō Jun'ichi</i>)</span>
+
+
+
+<div>
+     <p id="bounceTxt">Tooru (トール)</p>
+</div>
+
+    <style>
+    * {cursor: url(file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/Nyan Cat v2.ani), default;}
+    <style>
+
+cursor:url("file:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/animated.ico.ani")
+
+<p style="cursor: auto;">CSS Cursor: "auto"</p>
+
+<!-- Codes by HTMLcodes.ws -->
+<p style="cursor: auto;">CSS Cursor: "auto"</p>
+<p style="cursor: crosshair;">CSS Cursor: "crosshair"</p>
+<p style="cursor: default;">CSS Cursor: "default"</p>
+<p style="cursor: hand;">CSS Cursor: "hand" (Internet Explorer)</p>
+<p style="cursor: pointer;">CSS Cursor: "pointer" (All other browsers, plus Internet Explorer 6)</p>
+<p style="cursor: pointer; cursor: hand;">CSS Cursor: "pointer" and "hand" (cross-browser)</p>
+<p style="cursor: move;">CSS Cursor: "move"</p>
+<p style="cursor: e-resize;">CSS Cursor: "e-resize"</p>
+<p style="cursor: ne-resize;">CSS Cursor: "ne-resize"</p>
+<p style="cursor: nw-resize;">CSS Cursor: "nw-resize"</p>
+<p style="cursor: n-resize;">CSS Cursor: "n-resize"</p>
+<p style="cursor: se-resize;">CSS Cursor: "se-resize"</p>
+<p style="cursor: sw-resize;">CSS Cursor: "sw-resize"</p>
+<p style="cursor: s-resize;">CSS Cursor: "s-resize"</p>
+<p style="cursor: w-resize;">CSS Cursor: "w-resize"</p>
+<p style="cursor: text;">CSS Cursor: "text"</p>
+<p style="cursor: wait;">CSS Cursor: "wait"</p>
+<p style="cursor: progress;">CSS Cursor: "progress"</p>
+<p style="cursor: help;">CSS Cursor: "help"</p>
+<p style="cursor: url(http://www.htmlcodes.ws/images/links/cursor_move.gif),auto;">CSS Cursor: "url(cursor_move.gif),auto"</p>
+
+    <p style="cursor: url(ffile:///C:/Users/natas/OneDrive/Documents/études/NSI/Lycée/html/animated.ico.ani),auto;">CSS Cursor: "url(animated.ico),auto"</p>
+
+
+<marquee behavior="scroll" direction="left">Présentation en franglais de l'anime "Miss Kobayashi's Dragon Maid" </marquee>
+
+
+    <blockquote cite= "https://en.wikipedia.org/wiki/Fhána">
+    <p>Fhána (stylized as fhána ) is a four-member Japanese pop band that formed in 2012 as an indie band. 
+After releasing one album independently, they were signed in 2013 to Lantis. 
+They released their debut single "Que Sera Sera" in August 2013.</p>
+    <br>
+    <footer>—Fhána,' <cite><a href=https://en.wikipedia.org/wiki/Fhána>MyAnimeList</a></cite></footer>
+    </blockquote> 
+
+
+    <blockquote cite= "https://myanimelist.net/anime/33206/Kobayashi-san_Chi_no_Maid_Dragon?q=miss"
+ target="_blank">
+   Fhána (stylized as fhána ) is a four-member Japanese pop band that formed in 2012 as an indie band. 
+After releasing one album independently, they were signed in 2013 to Lantis. 
+They released their debut single "Que Sera Sera" in August 2013.
+<br> <br>
+<footer>—Fhána, <cite><a href=https://en.wikipedia.org/wiki/Fhána
+ target="_blank">Wikipedia</a></cite></footer>
+    </blockquote>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
